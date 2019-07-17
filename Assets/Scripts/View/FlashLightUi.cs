3c7b05e879fc54dca316887d375101701c5dd401
Кирилл Волков <success.vka@gmail.com>
2019-07-17 05:38:25 +0300

    Make

---
+++ Assets/Scripts/View/FlashLightUi.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Geekbrains
+{
+    public class FlashLightUi : MonoBehaviour
+    {
+	    private Text _text;
+        [SerializeField] private Image _image;
+
+        private void Awake()
+	    {
+		    _text = GetComponent<Text>();
+            //_image = GetComponent<Image>();
+	    }
+
+	    public float Text
+	    {
+		    set => _text.text = $"{value:0.0}";
+	    }
+        public float ChargeFill
+        {
+            set => _image.fillAmount = value;
+        }
+
+        public void SetActive(bool value)
+	    {
+		    _text.gameObject.SetActive(value);
+	    }
+    }
+}
---
+++ Assets/Scripts/View/GUIName.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Geekbrains
+{
+    public class GUIName : MonoBehaviour
+    {
+        public Transform cam1; // Камера
+        RaycastHit rch1;// луч который будет выходить из камеры
+        public bool _showName; // переменная
+        void Update()
+        {
+
+            Vector3 Direction = cam1.TransformDirection(Vector3.forward); //
+            if (Physics.Raycast(cam1.position, Direction, out rch1, 10)) //заставляет бить из нашей камеры луч на дистанцию равную 3
+            {
+                if (rch1.collider.GetComponent<Name>()) // и если этот луч встретился с коллайдером объекта на котором есть скрипт Name,
+                {
+                    _showName = true; // то переменная становится равна true.
+                }
+            }
+            else
+                _showName = false;
+        }
+        public void OnGUI()
+        {
+            if (_showName) // если переменная становится равна true
+            {
+                GUI.Label(new Rect((Screen.width) / 2, (Screen.height) / 2, 125, 25), " " + rch1.collider.GetComponent<Name>().nameGui); // то отображаем на экране персонажа, в указанном месте, значение nameGUI, которое берем из скрипта Name.
+            }
+        }
+    }
+}
